4feec81f1b35c733b846ae26f3f642e44de08b2d
Angel Kurten <angelkurten@hotmail.com>
2015-08-12 09:16:27 -0500

    Add Documentation

--- String.php
+++ String.php
@@ -4,7 +4,6 @@
 /**
  * Class String
  * @package AngelKurten\String
- *
  */
 class String {
 
@@ -13,6 +12,12 @@ class String {
     private $separator;
     private $parse = true;
 
+
+    /**
+     * @param $value
+     * @param bool|false $parse Is true convert all applicable characters to HTML entities
+     * @param string $separator Glue for implode if is array
+     */
     public function __construct($value, $parse = false, $separator = " ")
     {
         if(is_array($value))
@@ -30,60 +35,118 @@ class String {
         $this->parse = $parse;
     }
 
+    /**
+     * Get string length
+     *
+     * @return int
+     */
     public function length()
     {
-
         return strlen($this->string);
     }
 
+    /**
+     * Reverse a string
+     *
+     * @return $this
+     */
     public function invert()
     {
         $this->string = strrev($this->string);
         return $this;
     }
 
+
+    /**
+     * Make a string uppercase
+     *
+     * @return $this
+     */
     public function upper()
     {
         $this->string =  strtoupper($this->string);
         return $this;
     }
 
+    /**
+     * Make a string lowercase
+     *
+     * @return $this
+     */
     public function lower()
     {
         $this->string = strtolower($this->string);
         return $this;
     }
 
+    /**
+     * Uppercase the first character of each word in a string
+     *
+     * @return $this
+     */
     public function upWords()
     {
         $this->string = ucwords($this->string);
         return $this;
     }
 
+    /**
+     * Make a string's first character uppercase
+     *
+     * @return $this
+     */
     public function upFirst()
     {
         $this->string = ucfirst($this->string);
         return $this;
     }
 
+    /**
+     * Make a string's first character lowercase
+     *
+     * @return $this
+     */
     public function downFirst()
     {
         $this->string = lcfirst($this->string);
         return $this;
     }
 
-    public function fragment($limit, $break = "<br />", $cut = true)
+    /**
+     * Wraps a string to a given number of characters
+     *
+     * @param integer $limit Number of characters at which the string will be wrapped.
+     * @param string $break The line is broken using the optional break parameter.
+     * @param bool|true $cut If the cut is set to TRUE, the string is always wrapped at or before the specified width. So if you have a word that is larger than the given width, it is broken apart. When FALSE the function does not split the word even if the width is smaller than the word width.
+     * @return $this
+     */
+    public function fragment($limit = 75, $break = "<br />", $cut = true)
     {
         $this->string = wordwrap($this->string, $limit, $break, $cut);
         return $this;
     }
 
-    public function replace($array)
+    /**
+     * Replace all occurrences of the search string with the replacement string
+     *
+     * @param array $array The Value being searched for, otherwise known as the needle. An array may be used to designate multiple needles.
+     * @return $this
+     */
+    public function replace(Array $array)
     {
-        $this->string = str_ireplace(array_keys($array), array_values($array), $this->string);
+        $this->string = str_replace(array_keys($array), array_values($array), $this->string);
         return $this;
     }
 
+    /**
+     * Return part of a string
+     *
+     * @param integer $start The input string. Must be one character or longer.
+     * @param null $length  If start is non-negative, the returned string will start at the start'th position in string, counting from zero. For instance, in the string 'abcdef', the character at position 0 is 'a', the character at position 2 is 'c', and so forth.
+                            If start is negative, the returned string will start at the start'th character from the end of string.
+                            If string is less than start characters long, FALSE will be returned.
+     * @return $this
+     */
     public function partial($start, $length = null)
     {
         if (is_null($length))
@@ -95,29 +158,66 @@ class String {
         return $this;
     }
 
+    /**
+     * Split a string by string
+     *
+     * @param string $delimiter The boundary string.
+     * @return array
+     */
     public function toArray($delimiter = " ")
     {
-        return explode($delimiter,$this->string);
+        return explode($delimiter, $this->string);
     }
 
 
+    /**
+     * One-way string hashing
+     *
+     * @param null $salt
+     * @return string
+     */
     public function hash($salt = null)
     {
-        return crypt($this->string, $salt);
+        if(is_null($salt))
+        {
+            $hash = crypt($this->string);
+        }
+        else
+        {
+            $hash = crypt($this->string, $salt);
+        }
+
+        return $hash;
     }
 
+    /**
+     * Convert all applicable characters to HTML entities
+     *
+     * @return $this
+     */
     public function HtmlToString()
     {
         $this->string = htmlentities($this->string, ENT_QUOTES | ENT_HTML5, "UTF-8");
         return $this;
     }
 
+    /**
+     * Convert all HTML entities to their applicable characters
+     *
+     * @return $this
+     */
     public function StringToHtml()
     {
         $this->string = html_entity_decode($this->string, ENT_QUOTES | ENT_HTML5, "UTF-8");
         return $this;
     }
 
+    /**
+     * Join array elements with a string
+     *
+     * @param The array or Strings for concat
+     * @return $this
+     */
     public function concat()
     {
         foreach(func_get_args() as $str)
@@ -133,14 +233,17 @@ class String {
         return $this;
     }
 
-
+    /**
+     * Return the string representation of the method object.
+     *
+     * @return string
+     */
     public function __toString()
     {
         if($this->parse)
         {
             return htmlentities($this->string);
         }
-
         return $this->string;
     }
 
